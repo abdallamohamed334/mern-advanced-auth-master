e3b89246324d9e63c3ac3770f0c8c10d1f4e5726
abdallamohamed334 <tallaey445@gmail.com>
2025-10-30 23:19:23 +0200

    KK

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -1,16 +1,21 @@
 import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookie = (res, userId) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "7d",
-	});
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
 
-	res.cookie("token", token, {
-		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
-		maxAge: 7 * 24 * 60 * 60 * 1000,
-	});
+    res.cookie("token", token, {
+        httpOnly: true,
+        
+        // 1.secure: لازم تكون true عشان نستخدم SameSite: None
+        secure: process.env.NODE_ENV === "production", 
+        
+        // 2. sameSite: لازم تكون None للسماح بالاتصال بين Vercel و Railway
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+        
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
-	return token;
+    return token;
 };
