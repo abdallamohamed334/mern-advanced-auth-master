3034f2a791b44d73c970a291345858be11fd0824
abdallamohamed334 <tallaey445@gmail.com>
2025-10-31 20:01:08 +0200

    kk

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const RAILWAY_BACKEND_BASE = "https://mern-advanced-auth-master-production.up.railway.app"; 
 
 const FINAL_API_BASE = import.meta.env.MODE === "development"
-    ? "http://localhost:5000"
+    ? "https://mern-advanced-auth-master-production.up.railway.app"
     : RAILWAY_BACKEND_BASE; 
 
 axios.defaults.withCredentials = true;
